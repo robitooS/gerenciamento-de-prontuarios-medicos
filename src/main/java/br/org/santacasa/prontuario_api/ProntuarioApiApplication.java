d613e0bb6afdc657aca7108586fb0e3e0d584516
Higor Robin <proghigorrobin@gmail.com>
2025-03-11 00:48:39 -0300

    feat: add main Spring Boot application class

--- src/main/java/br/org/santacasa/prontuario_api/ProntuarioApiApplication.java
+++ src/main/java/br/org/santacasa/prontuario_api/ProntuarioApiApplication.java
@@ -1,6 +1,5 @@
 package br.org.santacasa.prontuario_api;
 
-import br.org.santacasa.prontuario_api.models.Paciente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
